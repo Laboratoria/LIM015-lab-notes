135f1881a3df8691abca2f41378a7b5b5718243b
noelishernandezdg <noelishernandezdagraca@gmail.com>
2021-10-26 09:49:10 -0500

    Se intentó conexión con Firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// import { initializeApp } from 'firebase/app';
+// import * as firebase from "firebase/app";
+// import 'firebase/auth';
+  
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAYTa2hpAtBRDnQ3IkhhWl5D8vdb9Lt1Pk",
+//   authDomain: "notitas-57023.firebaseapp.com",
+//   projectId: "notitas-57023",
+//   storageBucket: "notitas-57023.appspot.com",
+//   messagingSenderId: "336545729678",
+//   appId: "1:336545729678:web:f286e58c02638b37b40ddf",
+//   measurementId: "G-E4G9PK2MFD"
+// };
+
+// firebase.initializeApp(firebaseConfig);
+// export default firebase;
+
