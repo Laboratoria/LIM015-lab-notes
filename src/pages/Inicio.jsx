743a4d4f544caa1ff4e6f33df954e1d0d18ff278
noelishernandezdg <noelishernandezdagraca@gmail.com>
2021-10-26 09:50:33 -0500

    Se creó componente para la página de incio

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from '../components/login/Login';
+import Register from '../components/register/Register';
+
+const Inicio = () => {
+  return (
+    <>
+      <Login />
+      <Register />
+    </>
+  );
+};
+
+export default Inicio;
