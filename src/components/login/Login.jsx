4b5e6fec08abab653e67b777d1a435ac74ef1853
noelishernandezdg <noelishernandezdagraca@gmail.com>
2021-10-27 12:38:54 -0500

    agg console.log

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -14,7 +14,10 @@ const Header = () => {
 const Login = () => {
   const [user] = useState(false);
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = data => {
+    console.log(data.email);
+    console.log(data.password);
+  } 
 
   return (
     <section className='sectionFormAndLogo'>
@@ -25,10 +28,10 @@ const Login = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
         <label className='labelLogin'>Correo</label>
-        <input className='inputFormLogin' type='email' defaultValue="" {...register("example")} />
+        <input className='inputFormLogin' type='email' defaultValue="" {...register("email")} />
       {/* include validation with required or other standard HTML validation rules */}
         <label className='labelLogin'>Contraseña</label>
-        <input className='inputFormLogin' type='password' {...register("exampleRequired", { required: true })} />
+        <input className='inputFormLogin' type='password' {...register("password", { required: true })} />
       {/* errors will return when field validation fails  */}
       {errors.exampleRequired && <span>This field is required</span>}
           <button className='buttonLogin' type="submit">ENTRAR</button>
