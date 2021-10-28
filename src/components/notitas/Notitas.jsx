2cec4306d242add07b5515eacf1becc9f673414c
noelishernandezdg <noelishernandezdagraca@gmail.com>
2021-10-27 20:53:38 -0500

    maquetación de textArea para escribir notas

--- src/components/notitas/Notitas.jsx
+++ src/components/notitas/Notitas.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import '../../components/notitas/notitas.css'
 
 
 const Notitas = () => {
   return (
-  <h1>Este es el home</h1>  
+    <section className='content'>
+      <section className='contenteInputTitle'>
+        <input type='title' className='notasTitle' name="title" placeholder='Título'/>
+      </section>
+      <section className='ContentTextArea'>
+        <textarea name="notas" className='notasTextArea' placeholder='Añadir Nota'></textarea>
+      </section>
+  </section> 
   );
 };
-
 export default Notitas;
