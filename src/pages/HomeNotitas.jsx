7e080dccdca7cda0b478800bf7c28312b90d2538
noelishernandezdg <noelishernandezdagraca@gmail.com>
2021-11-01 20:54:24 -0500

    Se creo el componente para home

---
+++ src/pages/HomeNotitas.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from '../components/login/Login';
+import Notitas from '../components/notitas/Notitas';
+
+const HomeNotitas = () => {
+  return (
+    <>
+      <Login />
+      <Notitas />
+    </>
+  );
+};
+
+export default HomeNotitas;
